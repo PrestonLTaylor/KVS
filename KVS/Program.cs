47fd4409bebaff9fded35eec940b9bcc13108de2
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-02-24 14:13:17 +0000

    feat: Add our KV repository as a transient service

--- KVS/Program.cs
+++ KVS/Program.cs
@@ -1,8 +1,12 @@
+using KVS.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // TODO: Configuring Swagger/OpenAPI -> https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddRepositories();
 
 var app = builder.Build();
 
